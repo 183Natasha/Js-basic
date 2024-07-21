49aa2e7c9919546b5483eda6c0440828b972fa14
183Natasha <183natasha@mail.ru>
2024-07-21 06:30:36 +0000

    5

---
+++ work/work.js
@@ -0,0 +1,28 @@
+// Методом prompt получите ответ пользователя на вопрос "Сколько будет 7 + или - 15?". Если ответ верен, выведите в консоли "Успех", если нет - "Вы робот!", а если он введёт "Я не робот", то тоже "Успех".
+
+
+const res = prompt("Сколько будет 7 + или - 15?");
+// console.log(res);
+switch(true) {
+    case res === 'Я не робот':
+    case Number(res) === 22:
+    case Number(res) === -8:
+        console.log("Успех");
+        break;
+    default:
+        console.log("ВЫ робот");
+}
+
+// if (res === 'Я не робот'){
+//     console.log("Успех");
+// } else {
+//     const resNum = Number(res);
+//     switch(resNum){
+//         case 22:
+//         case -8:
+//             console.log("Успех");
+//             break;
+//         default:
+//             console.log("ВЫ робот");
+//     }
+// }
