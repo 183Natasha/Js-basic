9e3defaec602dd090173b3f2d17658f4f9f4606f
183Natasha <183natasha@mail.ru>
2024-08-15 15:14:05 +0000

    work - учебные задачи

--- 4-navigator/navigator.js
+++
@@ -1,7 +0,0 @@
-// Данные
-const adressLat = 0;
-const adressLong = 0;
-const posicionLat = 3;
-const posicionLonf = 4;
-
-console.log(Math.sqrt((adressLat-posicionLat)**2+(adressLong-posicionLonf)**2));
