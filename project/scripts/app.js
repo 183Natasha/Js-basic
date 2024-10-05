15e0f20e5d3eaa3b122115b467319566f8bc6606
183Natasha <183natasha@mail.ru>
2024-10-05 11:51:14 +0000

    добавление задачи

--- project/scripts/app.js
+++ project/scripts/app.js
@@ -2,6 +2,7 @@
 
 let habbits = [];
 const HABBIT_KEY = 'HABBIT_KEY';
+let globalActiveHabbitId;
 
 /* page */
 const page = {
@@ -29,10 +30,26 @@ function loadData() {
 
 /*work with days */
 function addDays(event){
+	const form = event.target;
 	event.preventDefault();
-	console.log (event);
-	const data = new FormData(event.target);
-	console.log (data.get("comment"));
+	const data = new FormData(form);
+	const comment = data.get("comment");
+	form['comment'].classList.remove('error');
+	if (!comment){
+		form['comment'].classList.add('error');
+	};
+	habbits = habbits.map(habbit => {
+		if (habbit.id === globalActiveHabbitId){
+			return{
+				...habbit,
+				days: habbit.days.concat([{ comment }])
+			}
+		}
+		return habbit;
+	})
+	form['comment'].value = '';
+	rerender(globalActiveHabbitId);
+	saveData();
 }
 
 function saveData() {
@@ -90,6 +107,7 @@ page.content.nextDay.innerHTML = `День ${activeHabbit.days.length + 1}`
 }
 
 function rerender(activeHabbitId) {
+	globalActiveHabbitId = activeHabbitId;
 	const activeHabbit = habbits.find(habbit => habbit.id === activeHabbitId);
 	if (!activeHabbit) {
 		return;
